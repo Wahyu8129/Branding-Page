139a2c5ef76ea20722b92e2f85c80bde9fdea5cd
Wahyu8129 <dikaoim123@gmail.com>
2025-03-13 20:21:02 +0700

    Tambah efek Blur saat Membuka Menu dan Tambah beberapa scirpt JS

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+        // Fungsi untuk membuka menu slide
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("overlay").classList.add("active");
+        }
 
-function returnToHome() {
-    window.location.href = "index.html";
-}
+        // Fungsi untuk menutup menu slide
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("overlay").classList.remove("active");
+        }
 
-function openProfile() {
-    document.getElementById("profileHover").style.width = "250px";
-}
+        // Fungsi untuk membuka menu profile
+        function openProfile() {
+            document.getElementById("profileHover").style.width = "250px";
+            document.getElementById("overlay").classList.add("active");
+        }
 
-function closeProfile() {
-    document.getElementById("profileHover").style.width = "0";
-}
+        // Fungsi untuk menutup menu profile
+        function closeProfile() {
+            document.getElementById("profileHover").style.width = "0";
+            document.getElementById("overlay").classList.remove("active");
+        }
 
-function openAbout() {
-    document.getElementById("aboutHover").style.width = "250px";
-}
+        // Fungsi untuk membuka menu about
+        function openAbout() {
+            document.getElementById("aboutHover").style.width = "250px";
+            document.getElementById("overlay").classList.add("active");
+        }
 
-function closeAbout() {
-    document.getElementById("aboutHover").style.width = "0";
-}
+        // Fungsi untuk menutup menu about
+        function closeAbout() {
+            document.getElementById("aboutHover").style.width = "0";
+            document.getElementById("overlay").classList.remove("active");
+        }
+   
